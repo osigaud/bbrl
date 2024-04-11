e49c4a56bdf8659ddecc921821d8c5bbe46f480e
osigaud <Olivier.Sigaud@upmc.fr>
2024-04-11 06:25:24 +0200

    removed several useless pieces of code

--- src/bbrl/workspace.py
+++ src/bbrl/workspace.py
@@ -724,33 +724,6 @@ class Workspace:
             workspace.set_full(k, v)
         return workspace
 
-    def debug_transitions(self, truncated):
-        """ """
-        critic, done, action_probs, reward, action = self[
-            "critic", "env/done", "action_probs", "env/reward", "action"
-        ]
-        timestep = self["env/timestep"]
-        assert not done[
-            0
-        ].max()  # dones is must be always false in the first timestep of the transition.
-        # if not it means we have a transition (step final) => (step initial)
-
-        # timesteps must always follow each other.
-        assert (timestep[0] == timestep[1] - 1).all()
-
-        assert (
-            truncated[not done].sum().item() == 0
-        )  # when done is false, truncated is always false
-
-        if done[truncated].numel() > 0:
-            assert torch.amin(
-                done[truncated]
-            )  # when truncated is true, done is always true
-        assert reward[1].sum() == len(
-            reward[1]
-        ), "in cartpole, rewards are always 1"  # only 1 rewards
-
-
 class _SplitSharedWorkspace:
     """This is a view over a Workspace, restricted to particular batch dimensions.
     It is used when multiple agents are reading/writing in the same workspace
